7f300c4263bb3ece98b303f59e407426effa3d25
tinchorlando <martin._orlando@hotmail.com.ar>
2023-05-04 10:26:04 -0300

    cambios en funcionalidad e impresion

--- TP1_Consigna1_GrupoMate.py
+++ TP1_Consigna1_GrupoMate.py
@@ -19,6 +19,56 @@ esté compuesta sólo por letras, no están permitidos los números,
 espacios ni ningún carácter especial, y que sea de la longitud
 correcta para el turno.
 """
+def formateo_resultados(resultados):
+    '''
+    Recibe la lista de resultados y las pasa a un formato amigable para la consola. Retorna un string con formato
+    '''
+    resultados_a_imprimir = ""
+    for letra in resultados:
+        if letra:
+            resultados_a_imprimir +=f"[{letra}]"
+        else:
+            resultados_a_imprimir +=f"[ ]"
+    return resultados_a_imprimir
+def imprimir_resultados(abecedario_imprimir, resultados, aciertos, errores, palabra, definicion):
+    """
+    Funcion encargada de imprimir el rosco en pantalla
+    """
+    print(abecedario_imprimir)
+    resultado_imprimir = formateo_resultados(resultados)
+    print(resultado_imprimir + "\n\n")
+    
+    print(f"Aciertos: {aciertos}")
+    print(f"Errores: {errores}")
+    
+    print(f"Turno letra {palabra[0]} - Palabra de {len(palabra)} letras")
+
+    print(f"Definición: {definicion}")
+
+def valida_respuesta (aciertos,errores,indice,palabra,resultados):
+    """
+    Se pide al usuario que ingrese su respuesta y se realiza la validacion
+    asi como tambien se actualizar las variables que dependen de la respuesta
+    """
+    respuesta = input("Ingrese palabra: ")
+    while not respuesta.isalpha():
+        print("Respuesta inválida, intente nuevamente")
+        respuesta = input("Ingrese palabra: ")    
+    if (len(palabra) == len(respuesta) and palabra.lower() == respuesta.lower()):
+        resultados[indice]="a"
+        aciertos += 1
+        print("Palabra correcta")
+    else:
+        print(f"Palabra incorrecta - Respuesta: {palabra}")
+        resultados[indice]="e"
+        errores += 1
+    return (aciertos,errores)
+
+def impresion_final(aciertos):
+    print("-------------------------------------")
+    print('Impresion de diccionario y respuestas')
+    print(f"Puntaje final: {aciertos}")
+
 def pasapalabras():
     
     """
@@ -26,12 +76,11 @@ def pasapalabras():
     y se le da un formato a las mismas para despues poder imprimirlas.
     """
     ABECEDARIO = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-
+    resultados = []
     abecedario_imprimir = ""
-    resultados_imprimir = ""
     for letra in ABECEDARIO:
         abecedario_imprimir += f"[{letra}]"
-        resultados_imprimir += "[ ]"
+        resultados.append(" ")
         
     
     """
@@ -46,38 +95,14 @@ def pasapalabras():
     """
     Se llama a la funcion que se encarga de imprimir la interfaz al usuario
     """
-    imprimir_resultados(abecedario_imprimir, resultados_imprimir, aciertos, errores, palabra, definicion)
-    
-    """
-    Se pide al usuario que ingrese su respuesta y se realiza la validacion
-    asi como tambien se actualizar las variables que dependen de la respuesta
-    """
-    respuesta = input("Ingrese palabra: ")
-    if(len(palabra) == len(respuesta) and respuesta.isalpha()):
-        if (palabra.lower() == respuesta.lower()):
-            print("Palabra correcta")
-            aciertos += 1
-        else:
-            errores += 1
-            print(f"Palabra incorrecta - Respuesta: {palabra}")
-        indice += 1
-    else:
-        print("Respuesta no valida")
+    while indice < len(ABECEDARIO): 
+        imprimir_resultados(abecedario_imprimir, resultados, aciertos, errores, palabra, definicion)
+        #valida respuesta y actualiza aciertos y errores
+        (aciertos,errores)=valida_respuesta(aciertos,errores,indice,palabra,resultados)
+        indice+=1
+    impresion_final(aciertos)
 
 
-def imprimir_resultados(abecedario_imprimir, resultados_imprimir, aciertos, errores, palabra, definicion):
-    """
-    Funcion encargada de imprimir el rosco en pantalla
-    """
-    print(abecedario_imprimir)
-    print(resultados_imprimir + "\n\n")
-    
-    print(f"Aciertos: {aciertos}")
-    print(f"Errores: {errores}")
-    
-    print(f"Turno letra {palabra[0]} - Palabra de {len(palabra)} letras")
-
-    print(f"Definición: {definicion}")
 
 #def validar_formato_respuesta(palabra, respuesta):
     #return True if (len(palabra) == len(respuesta) and respuesta.isalpha()) else False
