0660c4f9bdbaf3eb0c3d0ef669ba7bfd6a596622
juanchii940 <nu.ezjuan9@gmail.com>
2023-05-13 21:30:03 -0300

    cambios de etapa_4

--- etapa_4.py
+++ etapa_4.py
@@ -26,6 +26,65 @@ palabras_seleccionadas= palabras_del_rosco(definiciones, letras_participantes())
 #4. Luego se armará el tablero que visualizará el usuario, y dará comienzo la partida,
 #implementando así, lo realizado en la etapa 1.
 
+def formateo_resultados(resultados):
+    '''
+    Recibe la lista de resultados y las pasa a un formato amigable para la consola. Retorna un string con formato
+    '''
+    resultados_a_imprimir = ""
+    for letra in resultados:
+        if letra:
+            resultados_a_imprimir +=f"[{letra}]"
+        else:
+            resultados_a_imprimir +=f"[ ]"
+    return resultados_a_imprimir
+def imprimir_resultados(abecedario_imprimir, resultados, aciertos, errores, palabra, definicion):
+    """
+    Funcion encargada de imprimir el rosco en pantalla
+    """
+    print(abecedario_imprimir)
+    resultado_imprimir = formateo_resultados(resultados)
+    print(resultado_imprimir + "\n\n")
+    
+    print(f"Aciertos: {aciertos}")
+    print(f"Errores: {errores}")
+    
+    print(f"Turno letra {palabra[0]} - Palabra de {len(palabra)} letras")
+
+    print(f"Definición: {definicion}")
+
+def valida_respuesta (aciertos,errores,indice,palabra,resultados):
+    """
+    Se pide al usuario que ingrese su respuesta y se realiza la validacion
+    asi como tambien se actualizar las variables que dependen de la respuesta
+    """
+    respuesta = input("Ingrese palabra: ")
+    while not respuesta.isalpha():
+        print("Respuesta inválida, intente nuevamente")
+        respuesta = input("Ingrese palabra: ")    
+    if (len(palabra) == len(respuesta) and palabra.lower() == respuesta.lower()):
+        resultados[indice]="a"
+        aciertos += 1
+        print("Palabra correcta")
+    else:
+        print(f"Palabra incorrecta - Respuesta: {palabra}")
+        resultados[indice]="e"
+        errores += 1
+    return (aciertos,errores)
+
+def impresion_final(aciertos):
+    print("-------------------------------------")
+    print('Impresion de diccionario y respuestas')
+    print(f"Puntaje final: {aciertos}")
+
+def palabras(count):
+    palabra= palabras_seleccionadas[count]
+    return palabra
+
+def funcion_definiciones (count):
+    palabra= palabras_seleccionadas[count]
+    definicion= definiciones[palabra]
+    return definicion
+
 
 def pasapalabras():
     
@@ -33,12 +92,12 @@ def pasapalabras():
     Se crean las variables abecedario donde se encuentran todas las letras
     y se le da un formato a las mismas para despues poder imprimirlas.
     """
-    LETRAS=letras_participantes()
+    LETRAS = letras_participantes()
+    resultados = []
     abecedario_imprimir = ""
-    resultados_imprimir = ""
     for letra in LETRAS:
         abecedario_imprimir += f"[{letra}]"
-        resultados_imprimir += "[ ]"
+        resultados.append(" ")
         
     
     """
@@ -47,55 +106,21 @@ def pasapalabras():
     aciertos = 0
     errores = 0
     indice = 0
-
-    """defino una funcion para la correspondiente palabra y su definicion"""
+    count = 0
+    palabra = palabras(count)
+    definicion = funcion_definiciones(count)
     
-    #Se llama a la funcion que se encarga de imprimir la interfaz al usuario
-    
-    posicion=0
-    imprimir_resultados(abecedario_imprimir, resultados_imprimir, aciertos, errores, posicion)
     
     """
-    Se pide al usuario que ingrese su respuesta y se realiza la validacion
-    asi como tambien se actualizar las variables que dependen de la respuesta
+    Se llama a la funcion que se encarga de imprimir la interfaz al usuario
     """
-    
-    respuesta = input("Ingrese palabra: ")
-    if(len(palabra(posicion)) == len(respuesta) and respuesta.isalpha()):
-        if (palabra(posicion).lower() == respuesta.lower()):
-            print("Palabra correcta")
-            aciertos += 1
-        else:
-            errores += 1
-            print(f"Palabra incorrecta - Respuesta: {palabra()}")
-        indice +=1
-        posicion +=1
-    else:
-        print("Respuesta no valida")
-
-def palabra(posicion):
-    return palabras_seleccionadas(posicion)
-
-def definicion(posicion):
-    return definiciones[palabra(posicion)]
-
-
-def imprimir_resultados(abecedario_imprimir, resultados_imprimir, aciertos, errores,posicion):
-        palabra1= palabra(posicion)
-        definicion1= definicion(posicion)
-        #Funcion encargada de imprimir el rosco en pantalla
-        print(abecedario_imprimir)
-        print(resultados_imprimir + "\n\n")
-
-        print(f"Aciertos: {aciertos}")
-        print(f"Errores: {errores}")
-
-        print(f"Turno letra {palabra1} - Palabra de {len(palabra1)} letras")
-
-        print(f"Definición: {definicion1}")
-
-def validar_formato_respuesta(palabra, respuesta):
-    return True if (len(palabra) == len(respuesta) and respuesta.isalpha()) else False
+    while indice < len(LETRAS): 
+        imprimir_resultados(abecedario_imprimir, resultados, aciertos, errores, palabra, definicion)
+        #valida respuesta y actualiza aciertos y errores
+        (aciertos,errores)=valida_respuesta(aciertos,errores,indice,palabra,resultados)
+        indice+=1
+        count +=1
+    impresion_final(aciertos)
 
 pasapalabras()
 
