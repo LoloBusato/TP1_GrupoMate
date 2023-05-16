38330cbcbae773626b1b85d504f025a93d475bc7
tinchorlando <martin._orlando@hotmail.com.ar>
2023-05-16 02:33:57 -0300

    cambio numeros magicos y agrego validacion de tildes

--- etapa_3.py
+++ etapa_3.py
@@ -1,24 +1,25 @@
-from etapa_2 import definiciones
-import random
-
-letras_rosco= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z']
-random.shuffle(letras_rosco)
-
+letras_tildadas = {"á":"a","é":"e","í":"i","ó":"o","ú":"u"}
 definiciones_rosco= []
-
+MAX_LETRAS_ROSCO = 10
 INICIAL = 0
 
 def palabras_del_rosco (diccionario,letras):
+    '''
+    recibe diccionario con formato {palabra: definicion} y listado de 10 letras participantes
+    '''    
     palabras_lista = list(diccionario.keys())
     for letra in letras:
         indice = 0
         encontrado = False
         while indice < len(palabras_lista) and not encontrado:
-            if palabras_lista[indice][INICIAL] == letra:
+            if palabras_lista[indice][INICIAL] == chequeo_tilde(letra):
                 encontrado = True
                 definiciones_rosco.append(palabras_lista[indice])
             indice += 1
     
     return sorted(definiciones_rosco)
 
-
+def chequeo_tilde(letra):
+    if letra in letras_tildadas.keys():
+        letra=letras_tildadas[letra]
+    return letra
