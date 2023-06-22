eabd9a44b37e7c33c2268f00c62792e75401b3cc
Lorenzo Busato <lorenzobusato07@gmail.com>
2023-06-22 17:44:15 -0300

    Add: agrego pre y post condiciones para la funcion obtener_constantes

--- etapa_10.py
+++ etapa_10.py
@@ -3,12 +3,24 @@ import csv
 NOMBRE = 0
 VALOR = 1
 def obtener_constantes():
+    # Hecha por Busato Lorenzo
+    """
+    * funcion obtener_constantes  - lee el archivo configuracion.csv y crea un diccionario de constantes
+    *
+    * pre: configuracion.csv es un archivo con formato NOMBRE, VALOR por linea
+    *
+    * post: devuelve el diccionario en formato
+    *       {
+    *           NOMBRE: VALOR,
+    *           ...
+    *       }
+    """
     constantes = {}
     with open('configuracion.csv', 'r') as archivo:
         configuracion = csv.reader(archivo)
-        for fila in configuracion:
-            variable = fila[NOMBRE]
-            valor = fila[VALOR]
+        for linea in configuracion:
+            variable = linea[NOMBRE]
+            valor = linea[VALOR]
             constantes[variable] = valor
 
     return constantes
