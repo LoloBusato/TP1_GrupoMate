6b843dade98e66b6887e8e4987e4fd092189a0ad
Lorenzo Busato <lorenzobusato07@gmail.com>
2023-06-22 19:07:15 -0300

    Add: agrego un post y pre a la uncion leer_archivo_diccionario

--- Etapa_8.py
+++ Etapa_8.py
@@ -14,34 +14,56 @@ def leer_archivos(palabras,definiciones):
     *
     * Post: devuelve la siguiente linea de los archivos que contienen las palabras y definiciones
     *       
-    *
     """
     linea_def = definiciones.readline().rstrip()
     linea_pal = palabras.readline().rstrip()
     return linea_pal,linea_def
 
 def crear_diccionario(palabras,definiciones):
-    dicc={}
-    palabra,definicion=leer_archivos(palabras,definiciones)
+    # Hecha por Nuñez Juan Bautista
+    """
+    * Funcion encargada de crear un diccionario con las palabras,definiciones
+    *
+    * Pre: recibe 2 archivos abiertos en formato .txt ordenados por linea
+    *
+    * Post: devuelve un diccionario con todas las definiciones y palabras con formato
+    *   
+    *       
+    """
+    dicc = {}
+    palabra,definicion = leer_archivos(palabras,definiciones)
     while palabra:
-            #valido que la palabra sea alum.
-            if palabra.isalnum() and len(palabra) > int(CONFIGURACION['LONGITUD_PALABRA_MINIMA']):
-                dicc[palabra]="'"+str(definicion)+"'"
+            if palabra.isalpha() and len(palabra) > int(CONFIGURACION['LONGITUD_PALABRA_MINIMA']):
+                dicc[palabra] = definicion
             palabra,definicion=leer_archivos(palabras,definiciones)
-    dicc=sorted(dicc.items(),key=lambda x:x[PALABRA])
+    dicc = sorted(dicc.items(),key=lambda x:x[int(CONFIGURACION['LONGITUD_PALABRA_MINIMA'])])
     return dicc
 
-#------------------------------------------------------------------------------------------------------------------------
 
-#CREO EL ARCHIVO DICCIONARIO.CSV
 def crear_diccionario_csv(palabras,definiciones,diccionario):
+    # Hecha por Nuñez Juan Bautista
+    """
+    * Funcion encargada de crear un archivo csv con las palabras, definicion
+    *
+    * Pre: Recibe 2 archivos abiertos de formato txt que contienen todas las palabras definicion
+    *   y 1 archivo abierto vacio en modo escritura
+    *       
+    """
     palabras_definiciones=crear_diccionario(palabras,definiciones)
     for palabra_definicion in palabras_definiciones:
-        diccionario.write(palabra_definicion[0] + ','  + palabra_definicion[1] + "\n")
+        diccionario.write(palabra_definicion[int(CONFIGURACION['PALABRA'])] + ','  + palabra_definicion[int(CONFIGURACION['DEFINICION'])] + "\n")
     
 
 def leer_archivo_diccionario(diccionario_txt):
-    linea=diccionario_txt.readline().rstrip()
+    # Hecha por Nuñez Juan Bautista
+    """
+    * Funcion encargada de leer cada linea del archivo diccionario.txt
+    *
+    * Pre: Recibe 1 archivo abierto en formato .txt
+    *
+    * Post: devuelve como lista una linea del archivo     
+    """
+    linea = diccionario_txt.readline().rstrip()
     return linea.split(',') if linea else ('','')
 
 
